1472ecc857d9f08dc9b9a3d2271185b16896f115
DnMasterox <kod1289000>
2020-02-28 20:23:48 +0200

    Work with packages status

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :couriers   do
-    resources :packages
+    resources :packages do
+      member do
+        patch :complete
+      end
+    end
   end
   root 'couriers#index'
 end
